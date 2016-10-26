0798d991a16928e05f6cc74a40ac695056de4eb2
mybsktb <mybsktb@163.com>
2016-10-26 23:00:15 +0800

    login demo

--- src/com/lxsh/controller/UserCtrl.java
+++ src/com/lxsh/controller/UserCtrl.java
@@ -5,11 +5,14 @@ import javax.annotation.Resource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
+import com.lxsh.common.Const;
 import com.lxsh.model.User;
 import com.lxsh.service.IUserService;
 
 @Controller
+@SessionAttributes("username")
 public class UserCtrl {
 	
 	@Resource(name="userService")
@@ -20,9 +23,18 @@ public class UserCtrl {
 	}
 
 	@RequestMapping("/login")
-	public String login(int id, Model model){
-		User user = userService.getUserById(id);
-		model.addAttribute("username", user.getUsername());
-		return "index";
+	public String login(User user, Model model){
+		System.out.println(user);
+		String result = userService.login(user);
+		if(Const.SUCCESS.equals(result)){
+			model.addAttribute("username", user.getUsername());
+			return "index";
+		} else if(Const.NOT_EXIST.equals(result)){
+			model.addAttribute("info", "用户名或密码错误！");
+			return "login";
+		} else {
+			model.addAttribute("info", "登录失败！");
+			return "login";
+		}
 	}
 }
